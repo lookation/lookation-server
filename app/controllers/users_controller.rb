5e4346a68d2d1baa2b4ffbc96e46d9088a5400ed
bsvin33t <bsv.1511@gmail.com>
2015-04-25 18:15:19 +0530

    added controller and rendering JSON for the corrosponding controllers, added Readme

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,31 @@
 class UsersController < ApplicationController
+
+  protect_from_forgery with: :null_session
+
   def create
-    User.where(PhoneNumber.new(permit_params[:phone_number]).phone_number).first_or_create do |user|
-      user.latitude = permit_params[:latitude]
-      user.longitude = permit_params[:longitude]
+    User.where(PhoneNumber.new(user_params[:phone_number]).phone_number).first_or_create do |user|
+      user.phone_number = user_params[:phone_number]
+      user.latitude = user_params[:latitude]
+      user.longitude = user_ params[:longitude]
     end
+    render json: {message: "success"}
   end
 
   def update
-    phone_number = PhoneNumber.new(permit_params[:phone_number]).phone_number
-    User.where(phone_number)
-        .first
-        .update_attributes(longitude: permit_params[:longitude], latitude: permit_params[:latitude])
+    phone_number = PhoneNumber.new(user_params[:phone_number]).phone_number
+    user = User.where(phone_number)
+               .first
+               .update_attributes(longitude: user_params[:longitude], latitude: user_params[:latitude])
+    if user.nil?
+      render json: {message: "success"}
+    else
+      render status: :not_found, json: {message: "user not found"}
+    end
   end
 
   private
 
-  def permit_params
-    params.permit(:phone_number, :latitude, :longitude)
+  def user_params
+    params.require(:users).permit(:phone_number, :latitude, :longitude)
   end
 end
